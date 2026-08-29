50ddda4cd8e7cfc9bd3c2e824f98fdc58089d86a
sharafboyevislom-sketch <sharafboyevislom@gmail.com>
2026-08-29 17:39:21 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -126,6 +126,13 @@ bot.on("web_app_data", (msg) => {
 // ==== HTTP server: Supabase'dan "yangi buyurtma" webhook'ini qabul qilish ====
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, x-webhook-secret");
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+  next();
+});
 
 app.get("/", (req, res) => res.send("Bot ishlayapti."));
 
